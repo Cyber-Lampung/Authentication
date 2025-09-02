eece26b2cadc27ba26f20c3b0be319c71a57505a
Rado Hidayatulloh <acountgithub20@gmail.com>
2025-09-02 23:25:00 +0700

    fix: *

--- src/service/authService.js
+++ src/service/authService.js
@@ -5,7 +5,11 @@ export async function RegisterPost(email, username, password) {
     "https://backend-dusky-eight-18.vercel.app/Register",
     {
       method: "POST",
-      headers: { "Content-Type": "applicatio/json", authorization: "dia" },
+      headers: {
+        "Content-Type": "applicatio/json",
+        authorization: "dia",
+        "Access-Control-Allow-Origin": "*",
+      },
       body: JSON.stringify({
         id: id,
         email: email,
